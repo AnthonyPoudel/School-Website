bac4ddb5fb4b1668a35ba0d874f0c86937e3903a
AnthonyPoudel <sacarologist@gmail.com>
2023-07-11 19:20:11 +0545

    uploading the project

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StudentList()
+        {
+            List<Student> stdList = new List<Student>
+            {
+                new Student{id=1,Name="Ram",Address="Itahari"},
+                new Student{id=2,Name="sita",Address="Itahari"},
+                new Student{id=3,Name="rita",Address="Itahari"},
+                new Student{id=4,Name="gita",Address="Itahari"}
+            };
+            return PartialView("_StudentList", stdList);
+        }
+        public IActionResult StudentListView()
+        {
+            return View();
+        }
+
+        public IActionResult Feature()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace WebProject.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
